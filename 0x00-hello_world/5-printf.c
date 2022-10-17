06fa3f78b76faf201887934a60dfe39173043727
Ibkae <icecool2015@gmail.com>
2022-10-17 14:25:57 -0700

    Proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,7 @@
 */
 int main(void)
 {
-	printf("with proper gramma, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+		;
 }
